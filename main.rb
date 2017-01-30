0f54fdfe0d4367c1c7a65375755a72a91b73979c
Greg Woodcock <greg@yoomee.com>
2017-01-30 13:01:58 +0000

    Implement more operations and refactor

--- main.rb
+++ main.rb
@@ -3,17 +3,28 @@ require 'byebug'
 class VM
   OPERATIONS = {
     0 => :halt,
-    19 => :out,
-    21 => :noop,
+    1 => :set,
+    2 => :push,
+    3 => :pop,
+    4 => :eq,
+    5 => :gt,
     6 => :jmp,
     7 => :jt,
-    8 => :jf
+    8 => :jf,
+    9 => :add,
+    12 => :and,
+    13 => :or,
+    14 => :not,
+    17 => :call,
+    19 => :out,
+    21 => :noop
   }.freeze
 
   def initialize(machine)
     @machine = machine
     @instructions = File.read(@machine).unpack('v*')
     @registers = []
+    @stack = []
     @index = 0
     @next_instruction = nil
   end
@@ -56,6 +67,57 @@ class VM
                         end
   end
 
+  def set
+    register, value = register_and_value
+    @registers[register] = value
+  end
+
+  def add
+    register, v1, v2 = register_and_two_values
+    value = (v1 + v2) % 32_768
+    @registers[register] = value
+  end
+
+  def eq
+    register, v1, v2 = register_and_two_values
+    value = v1 == v2 ? 1 : 0
+    @registers[register] = value
+  end
+
+  def push
+    @stack.push(next_instruction)
+  end
+
+  def pop
+    @registers[register] = @stack.pop
+  end
+
+  def gt
+    register, v1, v2 = register_and_two_values
+    @registers[register] = v1 > v2 ? 1 : 0
+  end
+
+  def and
+    register, v1, v2 = register_and_two_values
+    @registers[register] = v1 & v2
+  end
+
+  def or
+    register, v1, v2 = register_and_two_values
+    @registers[register] = v1 | v2
+  end
+
+  def not
+    register, value = register_and_value
+    @registers[register] = (~ value) & 32_767
+  end
+
+  def call
+    @stack.push @index
+    jump_point = next_instruction
+    @next_instruction = jump_point
+  end
+
   def noop
   end
 
@@ -64,14 +126,36 @@ class VM
   end
 
   def not_implemented
-    puts "NOT_IMPLEMENTED"
+    puts "NOT_IMPLEMENTED: #{@instructions[@index]}"
   end
 
   private
 
   def next_instruction
     @index += 1
-    @instructions[@index]
+    ni = @instructions[@index]
+    if ni >= 32_768
+      ni = get_register(ni)
+    end
+    ni
+  end
+
+  def register
+    @index += 1
+    register_index = 32_775 - @instructions[@index]
+    register_index
+  end
+
+  def register_and_value
+    [register, next_instruction]
+  end
+
+  def register_and_two_values
+    [*register_and_value, next_instruction]
+  end
+
+  def get_register(register)
+    @registers[(32_775 - register)] || 0
   end
 end
 
