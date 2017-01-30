e6743b2a30868cf51807c315765ae12ec03d4bd4
Greg Woodcock <greg@yoomee.com>
2017-01-30 17:27:36 +0000

    More ops

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'byebug'
+require 'io/console'
 
 class VM
   OPERATIONS = {
@@ -12,21 +13,28 @@ class VM
     7 => :jt,
     8 => :jf,
     9 => :add,
+    10 => :mult,
+    11 => :mod,
     12 => :and,
     13 => :or,
     14 => :not,
+    15 => :rmem,
+    16 => :wmem,
     17 => :call,
+    18 => :ret,
     19 => :out,
+    20 => :in,
     21 => :noop
   }.freeze
 
   def initialize(machine)
+    @debug = ARGV[0] == "true"
     @machine = machine
     @instructions = File.read(@machine).unpack('v*')
     @registers = []
     @stack = []
-    @index = 0
     @next_instruction = nil
+    @log = []
   end
 
   def run
@@ -34,7 +42,7 @@ class VM
   end
 
   def execute(instructions)
-    @index = 0
+    @index = 1
     while @index < instructions.count
       @next_instruction = nil
       send(OPERATIONS[instructions[@index]] || :not_implemented)
@@ -43,14 +51,17 @@ class VM
   end
 
   def out
+    @log << "OUT - #{@index}"
     print next_instruction.chr
   end
 
   def jmp
+    @log << "JMP - #{@index}"
     @next_instruction = next_instruction
   end
 
   def jt
+    @log << "JT - #{@index}"
     ni = next_instruction
     @next_instruction = if ni.zero?
                           @index + 2
@@ -60,6 +71,7 @@ class VM
   end
 
   def jf
+    @log << "JF - #{@index}"
     @next_instruction = if next_instruction.zero?
                           next_instruction
                         else
@@ -68,65 +80,135 @@ class VM
   end
 
   def set
+    @log << "SET - #{@index}"
     register, value = register_and_value
     @registers[register] = value
   end
 
   def add
+    @log << "ADD - #{@index}"
     register, v1, v2 = register_and_two_values
     value = (v1 + v2) % 32_768
     @registers[register] = value
   end
 
+  def mult
+    @log << "MULT - #{@index}"
+    register, v1, v2 = register_and_two_values
+    @registers[register] = (v1 * v2) % 32_768
+  end
+
   def eq
+    @log << "EQ - #{@index}"
     register, v1, v2 = register_and_two_values
     value = v1 == v2 ? 1 : 0
     @registers[register] = value
   end
 
   def push
+    @log << "PUSH - #{@index}"
     @stack.push(next_instruction)
   end
 
   def pop
+    @log << "POP - #{@index}"
     @registers[register] = @stack.pop
   end
 
   def gt
+    @log << "GT - #{@index}"
     register, v1, v2 = register_and_two_values
     @registers[register] = v1 > v2 ? 1 : 0
   end
 
   def and
+    @log << "AND - #{@index}"
     register, v1, v2 = register_and_two_values
     @registers[register] = v1 & v2
   end
 
+  def mod
+    @log << "MOD - #{@index}"
+    register, v1, v2 = register_and_two_values
+    @registers[register] = (v1 % v2)
+  end
+
   def or
+    @log << "OR - #{@index}"
     register, v1, v2 = register_and_two_values
     @registers[register] = v1 | v2
   end
 
   def not
+    @log << "NOT - #{@index}"
     register, value = register_and_value
     @registers[register] = (~ value) & 32_767
   end
 
+  def rmem
+    @log << "RMEM - #{@index}"
+    register, value = register_and_value
+    @registers[register] = @instructions[value]
+  end
+
+  def wmem
+    @log << "WMEM - #{@index}"
+    # key, value = two_values
+    # @memory[key] = value
+    register, value = [next_instruction, next_instruction]
+    @instructions[register] = value
+  end
+
   def call
-    @stack.push @index
+    @log << "CALL - #{@index}"
+    @stack.push @index + 2
     jump_point = next_instruction
     @next_instruction = jump_point
   end
 
+  def ret
+    @log << "RET - #{@index}"
+    @next_instruction = @stack.pop
+  end
+
+  def in
+    # in: 20 a
+    #   read a character from the terminal and write its ascii code to <a>;
+    #   it can be assumed that once input starts, it will continue until a newline is encountered;
+    #   this means that you can safely read whole lines from the keyboard and trust that they will be fully read
+    @log << "IN - #{@index}"
+
+    char = $stdin.getch
+    puts char
+    @instructions[next_instruction] = char.ord
+    # puts line
+    # char = STDIN.getc
+    # halt unless char
+    # code = char.ord
+    # instruction = raw_instruction
+    # register, value = register_and_value
+    # @memory[register] = value
+    # if register? instruction
+    #   key = register_id instruction
+    #   @registers[key] = code
+    # else
+    #   @memory[instruction] = code
+    # end
+  end
+
+
   def noop
+    @log << "NOOP - #{@index}"
   end
 
   def halt
+    @log << "HALT - #{@index}"
+    @log.each { |x| puts x } if @debug
     exit
   end
 
   def not_implemented
-    puts "NOT_IMPLEMENTED: #{@instructions[@index]}"
+    @log << "NOT_IMPLEMENTED - #{@instructions[@index]}"
   end
 
   private
